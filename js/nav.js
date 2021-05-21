debeefd81631700462a5193f8e6d680f43bdce39
mrevas <mrevas919@gmail.com>
2021-05-21 02:30:25 -0400

    start aligning the menu

--- js/nav.js
+++ js/nav.js
@@ -4,7 +4,8 @@ const burger = document.getElementById('burger');
 function toggleHamburger() {
     if (nav.style.display === 'block') {
         nav.style.display = 'none'
-    } else { nav.style.display = 'block'}
+    } else if (nav.style.display === none) {
+        nav.style.display = 'block'}
 }
 
 burger.addEventListener('click', toggleHamburger)
