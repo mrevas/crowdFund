d95ecd44ac39a44881038e4f01c175d635cab070
mrevas <mrevas919@gmail.com>
2021-05-30 18:53:45 -0400

    add more functionality to the modal

--- js/modal.js
+++ js/modal.js
@@ -1,13 +1,19 @@
+const modalDivs = document.getElementsByClassName('modalDivs')
 const closeModal = document.getElementById('close-modal')
 const body = document.getElementById('body')
+const radios = document.getElementsByClassName('radios')
+const enterPledge = document.getElementsByClassName('enterPledge')
 let modal = document.getElementsByClassName('modal-default')
-
 modal = modal[0];
+
+
 
 closeModal.addEventListener('click', () => {
         modal.style.display = 'none'
         body.style.overflow = 'auto'
 })
+
+
 
 for (i = 0; i < buttons.length; i ++) {
         if (i !== 1) {
@@ -17,3 +23,12 @@ for (i = 0; i < buttons.length; i ++) {
         })
     }
 }
+
+function showPledge(x) {
+        for(i = 0; i < enterPledge.length; i++) {
+                const enterPledge = document.getElementsByClassName('enterPledge')
+                enterPledge[i].style.display = 'none'        
+        }
+                enterPledge[x].style.display = 'block'
+}
+
