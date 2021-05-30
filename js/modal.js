4356a944afb014bc65dd7819e91cabe3f7b08f4f
mrevas <mrevas919@gmail.com>
2021-05-30 12:42:39 -0400

    disable body overflow when modal is open

--- js/modal.js
+++ js/modal.js
@@ -1,15 +1,19 @@
 const closeModal = document.getElementById('close-modal')
+const body = document.getElementById('body')
 let modal = document.getElementsByClassName('modal-default')
+
 modal = modal[0];
 
 closeModal.addEventListener('click', () => {
         modal.style.display = 'none'
+        body.style.overflow = 'auto'
 })
 
 for (i = 0; i < buttons.length; i ++) {
         if (i !== 1) {
         buttons[i].addEventListener('click', () => {
                 modal.style.display = 'flex'
+                body.style.overflow = 'hidden'
         })
     }
 }
