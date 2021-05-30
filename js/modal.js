eebba9357af0dece3e464585ead0fc56713f4546
mrevas <mrevas919@gmail.com>
2021-05-30 11:58:49 -0400

    further add modal style changes

--- js/modal.js
+++ js/modal.js
@@ -1,5 +1,10 @@
+const closeModal = document.getElementById('close-modal')
 let modal = document.getElementsByClassName('modal-default')
 modal = modal[0];
+
+closeModal.addEventListener('click', () => {
+        modal.style.display = 'none'
+})
 
 for (i = 0; i < buttons.length; i ++) {
         if (i !== 1) {
